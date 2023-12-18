be150b1d510c23938af6b07efbefd15edb2e6790
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-12-18 16:48:00 +1100

    feat: login and signup page functionality started

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 const Login = () => {
-  return <h1>Login Page</h1>;
+    
+    const navigate = useNavigate();
+    const [jwt, setJwt] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    useEffect(() => {
+        console.log(`JWT value is:\n${jwt}`);
+    }, [jwt]);
+
+    async function login(){
+        console.log(email, password);
+        try{
+            let result = await axios.post(process.env.REACT_APP_BACKEND_URL + "users/sign-in", {
+                email,
+                password,
+                });
+        } catch (err) {
+            toast.error((err));
+        }
+
+
+    }
+    
+    return (
+    <div className="form_container">
+        <h2>Login Account</h2>
+        <form onSubmit={(event) => {event.preventDefault(); login(); }}>
+        <div>
+            <label htmlFor="email">Email</label>
+            <input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={(event) => setEmail(event.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="password">Password</label>
+            <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={(event) => setPassword(event.target.value)}
+            />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+            Already have an account? <Link to={"/signup"}>Signup</Link>
+        </span>
+        </form>
+        <ToastContainer />
+    </div>
+    );
 };
 
 export default Login
