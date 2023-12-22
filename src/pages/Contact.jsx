3154f124e53be3269da40568dce76b143d30b148
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-12-22 22:35:25 +1100

    feat: added seed images for opals

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import '../styling/Contact.css'
 
 
 const Contact = () => {
 
     return (
-        <div>
-            <h2>Behind Tesselate</h2>
-            <p>We would love to hear from you!</p>
-            <p>Send us a message by emailing tesselatejewellery@gmail.com</p>
-            <p>We will get back to you as soon as possible.</p>
+        <div className="contact-text">
+            <div>
+                <h1>CONTACT US</h1>
+            </div>
+            <div>
+                <p>We would love to hear from you!</p>
+                <p>Send us a message by emailing <strong>tesselatejewellery@gmail.com</strong></p>
+                <p>We will get back to you as soon as possible.</p>
+            </div>
         </div>
     );
 };
--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -7,77 +7,77 @@ const Faq = () => {
             <h2>Frequently Asked Questions</h2>
 
             <div>
-                <h3>How can I acquire an opal piece?</h3>
+                <h3 className="faq-header">How can I acquire an opal piece?</h3>
                 <p>
                     When sending a quote, pick your preferred metal from the options and provide specific details such as the desired setting, design, and ring size during the checkout process.
                 </p>
             </div>
 
             <div>
-                <h3>Are your opals genuinely natural?</h3>
+                <h3 className="faq-header">Are your opals genuinely natural?</h3>
                 <p>
                     Absolutely, our collection mainly features high-quality, naturally sourced Australian opals, untouched by any artificial processes.
                 </p>
             </div>
 
             <div>
-                <h3>Is the sourcing of your opals ethical?</h3>
+                <h3 className="faq-header">Is the sourcing of your opals ethical?</h3>
                 <p>
                     Yes, our opals are ethically sourced from trusted independent miners and cutters, primarily originating from South Australia and New South Wales.
                 </p>
             </div>
 
             <div>
-                <h3>Can opals be exposed to water?</h3>
+                <h3 className="faq-header">Can opals be exposed to water?</h3>
                 <p>
                     Indeed, solid Australian opals can be submerged in water without undergoing any color alterations.
                 </p>
             </div>
 
             <div>
-                <h3>May I use my opal for custom jewelry?</h3>
+                <h3 className="faq-header">May I use my opal for custom jewelry?</h3>
                 <p>
                     We appreciate your interest, but currently, we're unable to accept external opals for custom jewelry. Our commitment to quality craftsmanship and the unique characteristics of our sourced opals guides our process. If you have any specific inquiries or need assistance, feel free to reach out, and we'll do our best to help.
                 </p>
             </div>
 
             <div>
-                <h3>What materials do you use for the metal?</h3>
+                <h3 className="faq-header">What materials do you use for the metal?</h3>
                 <p>
                     We exclusively use genuine sterling silver, along with 9 and 18 ct gold. Our metals are recycled and locally sourced within Australia.
                 </p>
             </div>
 
             <div>
-                <h3>How long does it take to receive a customized piece?</h3>
+                <h3 className="faq-header">How long does it take to receive a customized piece?</h3>
                 <p>
                     We typically request about 4 weeks for the crafting process. Once ready, we ship it with tracking via Australia Post's express service for a speedy delivery.
                 </p>
             </div>
 
             <div>
-                <h3>What's the best way to care for my opal jewelry?</h3>
+                <h3 className="faq-header">What's the best way to care for my opal jewelry?</h3>
                 <p>
                     Find guidance in our care guide or reach out to us via email for detailed information on maintaining your opal pieces. Every purchase includes care instructions.
                 </p>
             </div>
 
             <div>
-                <h3>Can I make changes to my order details?</h3>
+                <h3 className="faq-header">Can I make changes to my order details?</h3>
                 <p>
                     Changes to design specifics, including ring size, can be accommodated only if we haven't initiated the production of your piece. Reach out to us with any concerns.
                 </p>
             </div>
 
             <div>
-                <h3>How can I determine my ring size?</h3>
+                <h3 className="faq-header">How can I determine my ring size?</h3>
                 <p>
                     For the most accurate sizing, we recommend visiting a local jeweler.
                 </p>
             </div>
 
             <div>
-                <h3>What should I do if my piece arrives with issues?</h3>
+                <h3 className="faq-header">What should I do if my piece arrives with issues?</h3>
                 <p>
                     Notify us immediately, and we'll do our best to address and rectify any problems with your piece.
                 </p>
